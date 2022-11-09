d38705823e4b53c411dfd6a3635cc8ddfcd72363
liqian <791469353@qq.com>
2022-11-09 19:08:14 +0800

    add city-ajax

--- src/components/city/List.vue
+++ src/components/city/List.vue
@@ -12,109 +12,26 @@
             <div class="area">
                 <div class="title  border-bottom">热门城市</div>
                 <div class="button-list">
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
+                    <div
+                    class="button-wrapper"
+                    v-for="item of hotCities"
+                    :key="item.id">
+                        <div class="button">{{ item.name }}</div>
                     </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
+
                 </div>
             </div>
-            <div class="area">
-                <div class="title  border-bottom">A</div>
+            <div class="area" v-for="(item, key) in cities" :key="key">
+
+                <div class="title  border-bottom"> {{ key }}</div>
                 <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
+                    <div
+                    class="item border-bottom"
+                    v-for="innerItem of item"
+                    :key="innerItem.id">
+                        {{ innerItem.name }}
                     </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title  border-bottom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title  border-bottom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title  border-bottom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title  border-bottom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
-                    <div class="item border-bottom">
-                        阿拉尔
-                    </div>
+
                 </div>
             </div>
         </div>
@@ -127,8 +44,21 @@ import Bscroll from 'better-scroll'
 
 export default {
     naem:'CityList',
+    props: {
+        hotCities: Array,
+        cities: Object
+    },
+    watch: {
+        cities () {
+            this.$nextTick(() => {
+                this.scroll.refresh()
+            })
+        }
+    },
     mounted() {
+
         this.scroll = new Bscroll(this.$refs.wrapper)
+
     }
 }
 </script>
@@ -140,7 +70,7 @@ export default {
             border-top-color: #ccc;
         }
         &::after {
-            border-top-color: #ccc;
+            border-bottom-color: #ccc;
         }
     }
 
--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,8 +2,8 @@
     <div class="city">
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 
 </template>
@@ -13,6 +13,8 @@ import CityHeader from '@/components/city/Header.vue'
 import CitySearch from '@/components/city/Search.vue'
 import CityList from '@/components/city/List.vue'
 import CityAlphabet from '@/components/city/Alphabet.vue'
+
+import axios from 'axios'
 export default {
     name: 'City',
     components: {
@@ -20,6 +22,36 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data () {
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    methods: {
+        getCityInfo () {
+            axios.get('/api/city.json')
+            .then(this.handleGetCityInfoSucc)
+            .catch(err=>{
+                console.log(err);
+            })
+        },
+        handleGetCityInfoSucc(res) {
+            res = res.data
+
+            if( res.ret && res.data ) {
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+
+            }
+
+        }
+    }
+    ,
+    mounted() {
+        this.getCityInfo()
     }
 }
 </script>
