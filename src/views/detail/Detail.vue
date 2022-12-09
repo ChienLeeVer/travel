9a0b16e1e167cd1484194730f43bf2ed951e602a
liqian <791469353@qq.com>
2022-12-09 11:32:32 +0800

    add

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -9,7 +9,7 @@
         </detail-header>
 
         <div class="content">
-
+            <detail-list :list="list"></detail-list>
         </div>
 
     </div>
@@ -18,12 +18,35 @@
 <script>
 import DetailBanner from '@/components/detail/Banner'
 import DetailHeader from '@/components/detail/Header'
+import DetailList from '@/components/detail/List'
 
 export default {
     name: 'Detail',
     components: {
         DetailBanner,
-        DetailHeader
+        DetailHeader,
+        DetailList
+    },
+    data() {
+        return {
+            list: [{
+                title: '成人票',
+                children: [{
+                    title: '成人三馆联票',
+                    children: [{
+                        title: '成人三馆联票 - 某一连锁店销售',
+                    }]
+                }, {
+                    title: '成人无馆联票'
+                }]
+            }, {
+                title: '学生票'
+            }, {
+                title: '儿童票'
+            }, {
+                title: '特惠票'
+            }]
+        }
     }
 }
 </script>
